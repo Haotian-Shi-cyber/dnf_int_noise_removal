4f2d9eab036de40dce9954832121fbc7ccc2f228
Haotian-Shi-cyber <shihaotian1@outlook.com>
2022-08-05 14:11:58 +0100

    rm cmkae

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/haotian/Desktop/123/dnf_int_noise_removal
-# Build directory: /home/haotian/Desktop/123/dnf_int_noise_removal
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("audio_filter")
-subdirs("cvui")
